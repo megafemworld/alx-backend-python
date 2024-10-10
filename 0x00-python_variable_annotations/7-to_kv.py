56433f8ecd4ae5196aaeaf48783d937c1a0df7ad
Femi Mehalayese <megafemclick@gmail.com>
2024-10-10 19:45:26 +0100

    correct error 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,7 @@
 
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """to_kv: take in string k, and v. return tuple"""
-    return (k, v)
+    return (k, v**2)
