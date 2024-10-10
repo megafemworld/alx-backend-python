ac5a43c3f8c064eb65e81558907eecb7bd07bea2
Femi Mehalayese <megafemclick@gmail.com>
2024-10-10 19:42:03 +0100

    complex types

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""string and int/float to tuple"""
+
+
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """to_kv: take in string k, and v. return tuple"""
+    return (k, v)
