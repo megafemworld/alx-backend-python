d46ccf6934f1f755b5d590c9060780b2328ddf55
Femi Mehalayese <megafemclick@gmail.com>
2024-10-14 23:00:09 +0100

    mesure time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module that uses asyncio
+"""
+import time
+import asyncio
+from typing import List
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the total execution time for wait.
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    average_time_per_call = total_time / n
+    return average_time_per_call
