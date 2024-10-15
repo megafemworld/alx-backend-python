b15e67b1ef0bc718b45d6b64749c2ba10682c119
Femi Mehalayese <megafemclick@gmail.com>
2024-10-15 11:50:28 +0100

    task manager

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""function integer max_delay and returns a asyncio.Task"""
+import asyncio
+wait_random = __import__('2-measure_runtime').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Return an asyncio.Task for wait_random"""
+
+    return asyncio.create_task(wait_random(max_delay))
