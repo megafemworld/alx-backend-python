71d9e75caab5c7dddfb7f8679afdf3e406b16849
Femi Mehalayese <megafemclick@gmail.com>
2024-10-15 18:57:56 +0100

    1. Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,11 @@
     1-async_comprehension.py
 """
 import asyncio
-from typing import Generator
+from typing import List
 import random
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
-    return [_ async for _ in async_generator()]
+async def async_comprehension() -> List[float]:
+    return [gen async for gen in async_generator()]
